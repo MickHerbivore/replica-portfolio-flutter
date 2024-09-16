32ca7241f9bac758e2433a923f814862d14086cc
Miguel Muñoz Caro <miguelmc.dev@gmail.com>
2024-09-15 22:20:51 -0300

    feat: contact, fotter at the end

--- lib/screens/contact_screen.dart
+++ lib/screens/contact_screen.dart
@@ -14,6 +14,7 @@ class ContactScreen extends StatelessWidget {
         flexibleSpace: const AppBarMenu(),
       ),
       body: Container(
+        height: screenSize.height,
         decoration: const BoxDecoration(
           gradient: LinearGradient(
             colors: [Color.fromRGBO(47, 60, 79, 1), Colors.black],
@@ -21,75 +22,76 @@ class ContactScreen extends StatelessWidget {
             end: Alignment.bottomCenter,
           ),
         ),
-        child: SizedBox(
-          height:screenSize.height,
-          width: screenSize.width,
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Column(
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.only(bottom: 16.0),
-                        child: Text(
-                          'Contact me',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                          )
-                        ),
-                      ),
-
-                      const ContactForm(),
-
-
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 32.0),
-                        child: Wrap(
-                          alignment: WrapAlignment.center,
-                          spacing: 8,
-                          runSpacing: 8,
-                          children: [
-                            const Text(
-                              'Or just send an email to',
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+              
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          const Padding(
+                            padding: EdgeInsets.only(bottom: 16.0),
+                            child: Text(
+                              'Contact me',
                               style: TextStyle(
                                 color: Colors.white,
-                                fontSize: 16,
-                                fontWeight: FontWeight.w700,
-                              ),
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold,
+                              )
                             ),
-                            GestureDetector(
-                              onTap: () {
-                                // launchURL('https://mickherbivore.com/contact#:~:text=miguelmc.dev%40gmail.com');
-                              },
-                              child: const Text(
-                                'miguelmc.dev@gmail.com',
-                                style: TextStyle(
-                                  color: Color.fromRGBO(167, 137, 243, 1),
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w700,
+                          ),
+              
+                          const ContactForm(),
+              
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 32.0),
+                            child: Wrap(
+                              alignment: WrapAlignment.center,
+                              spacing: 8,
+                              runSpacing: 8,
+                              children: [
+                                const Text(
+                                  'Or just send an email to',
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w700,
+                                  ),
                                 ),
-                              ),
-                            )
-                            
-                          ]
-                        ),
-                      ),
-                      
-                    ],
-                  )
-                ),
-                
-
-                const Footer()
-              ]
-            )
-          ),
+                                GestureDetector(
+                                  onTap: () {
+                                    // launchURL('https://mickherbivore.com/contact#:~:text=miguelmc.dev%40gmail.com');
+                                  },
+                                  child: const Text(
+                                    'miguelmc.dev@gmail.com',
+                                    style: TextStyle(
+                                      color: Color.fromRGBO(167, 137, 243, 1),
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                )
+                                
+                              ]
+                            ),
+                          ),
+                          
+                        ],
+                      )
+                    ),
+                    
+              
+                  ]
+                )
+              ),
+            ),
+            const Footer()
+          ],
         ),
       )
     );
